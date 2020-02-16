de02efe8903e244c518eff9b2dc66b9473028a77
Thiago Vieira <th91v@outlook.com>
2020-02-16 11:12:58 -0300

    componentes basicos, estilizacao da aplicacao e componentes

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# exchange_rate_calculator_react
-Repo educacional. Uma interface para calcular valores entre moedas internacionais.
---
+++ src/components/CurrencyRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CurrencyRow() {
+    return (
+        <div>
+            <input type="number" className="input"/>
+            <select>
+                <option value="BRL">BRL</option>
+            </select>
+        </div>
+    )
+}
