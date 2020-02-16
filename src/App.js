511f65b69c1b2c565e2bf9862893f6f251d3a0db
Thiago Vieira <th91v@outlook.com>
2020-02-16 12:31:00 -0300

    logica para conversao de valores

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ function App() {
   const [currencyOptions, setCurrencyOptions] = useState([]) // useState usa um array vazio pois nao ha opcoes quando a app carrega pela primeira vez
   const [fromCurrency, setFromCurrency] = useState();
   const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount; // as duas variaveis sao criadas de forma implicita
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate || 0;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
 
   useEffect(() => {
     fetch(API_URL)
@@ -17,8 +29,27 @@ function App() {
       setCurrencyOptions([data.base, ...Object.keys(data.rates)]) // insere nos options dos selescts as keys do array retornado como values
       setFromCurrency(data.base); // moeda da qual sao feitas as conversoes
       setToCurrency(firstCurrency); // moeda para qual serao feitas as conversoes
+      setExchangeRate(data.rates[firstCurrency]); // taxa de conversao a partir da primeira moeda
     })
   }, [])
+
+  useEffect(() => { // permite que ao selecionar um option diferente na interface, seja feita uma nova requisicao para atualizar taxas na tela
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`${API_URL}?base=${fromCurrency}&symbols=${toCurrency}`) // ver documentacao da API para entender esta requisicao
+      .then(res => res.json())
+      .then(data => setExchangeRate(data.rates[toCurrency])) 
+    }
+  }, [fromCurrency, toCurrency]) // se algum dos arrays passados for alterado, a funcao eh executada
+
+  function handleFromAmountChange(e){
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+  
+  function handleToAmountChange(e){
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
 
   return (
     <>
@@ -27,12 +58,16 @@ function App() {
         currencyOptions = {currencyOptions} // values dos options sendo passados como prop
         selectedCurrency = {fromCurrency} // value padrao para a option no select
         onChangeCurrency = {e => setFromCurrency(e.target.value)} // define value como o value selecionado no option da interface
+        onChangeAmount = {handleFromAmountChange}
+        amount = {fromAmount}
        />
       <div className="equals">=</div>
       <CurrencyRow
         currencyOptions = {currencyOptions} // values dos options sendo passados como prop
         selectedCurrency = {toCurrency} // value padrao para a option no select
         onChangeCurrency = {e => setToCurrency(e.target.value)} // define value como o value selecionado no option da interface
+        amount = {toAmount}
+        onChangeAmount = {handleToAmountChange}
       />
     </>
   );
--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default function CurrencyRow(props) {
-    const { currencyOptions, selectedCurrency, onChangeCurrency } = props; // faz destructuring da currencyOptions para props
+    const {
+        currencyOptions,
+        selectedCurrency,
+        onChangeCurrency,
+        amount,
+        onChangeAmount
+    } = props; // faz destructuring dos atributos para props
 
     return (
         <div>
-            <input type="number" className="input"/>
+            <input type="number" className="input" value={amount} onChange={onChangeAmount}/>
             <select value={selectedCurrency} onChange={onChangeCurrency}>
                 {currencyOptions.map(option => ( // map simplesmente retorna os valores de currencyOptions
                     <option key={option} value={option}>{option}</option>
